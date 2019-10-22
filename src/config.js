03e04369b3539c5d989d14e6c4dc9538f3e1dafa
Mike Blakley <m.blakley@icloud.com>
2019-10-21 23:03:26 -0400

    deployment modification to connect api

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `http://localhost:4050/api/bookmarks`,
+  API_ENDPOINT: `https://bookmarks-server-51072.herokuapp.com/`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
