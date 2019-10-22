e43f74188854a444bc282525e322b5ed07e1a2cd
Mike Blakley <m.blakley@icloud.com>
2019-10-22 08:21:12 -0400

    endpoint update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 export default {
-  API_ENDPOINT: `https://bookmarks-server-51072.herokuapp.com/`,
+  API_ENDPOINT: `https://bookmarks-server-51072.herokuapp.com/api/bookmarks`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
+
+// API_ENDPOINT: `https://bookmarks-server-51072.herokuapp.com/api/bookmarks`,
+// API_ENDPOINT: `http://localhost:4050/api/bookmarks`,
